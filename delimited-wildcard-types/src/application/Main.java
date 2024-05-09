8e78b5ec38c4995713c4e040cb7df4da7d8a4f6e
Filipemtb <filipera3@gmail.com>
2024-05-09 11:37:02 -0300

    problem solved using covariância e contravariância
    Fazer um método que copia os elementos de uma lista para uma outra lista que pode ser mais genérica que a primeira.

--- delimited-wildcard-types/src/application/Main.java
+++ delimited-wildcard-types/src/application/Main.java
@@ -1,65 +1,32 @@
 package application;
 
-import model.entities.Circle;
-import model.entities.Rectangle;
-import model.entities.Shape;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-       /* List<Shape> myShapes = new ArrayList<>();
-        myShapes.add(new Rectangle(3.0, 2.0));
-        myShapes.add(new Circle(2.0));
-
-
-        List<Circle> myCircles = new ArrayList<>();
-        myCircles.add(new Circle(2.0));
-        myCircles.add(new Circle(3.0));
-
-        System.out.println("Total area: " + totalArea(myShapes));
-    }
-
-    public static double totalArea(List<? extends Shape> list) {
-        double sum = 0.0;
-        for (Shape s : list) {
-            sum += s.area();
-        }
-        return sum;
-    }
-     */
-
-        /*
-         Princípio get/put - Covariância
-         Quando o GET é permitido mas o PUT não é permitido
-
-        List<Integer> intList = new ArrayList<>();
-        intList.add(10);
-        intList.add(5);
-
-        List<? extends Number> list = intList;
-
-        Number x = list.get(0);
-
-        list.add(20); // Erro de compilação
-
-        */
-
-
-         // Princípio get/put - Contravariância
-         // Quando o PUT é permitido mas o GET não é permitido
-
+        List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+        List<Double> myDoubles = Arrays.asList(3.14, 6.28);
         List<Object> myObjs = new ArrayList<>();
-        myObjs.add("Maria");
-        myObjs.add("Filipe");
 
-        List<? super Number> myNums = myObjs;
+        copy(myInts, myObjs);
+        printList(myObjs);
+        copy(myDoubles, myObjs);
+        printList(myObjs);
+    }
 
-        myNums.add(10);
-        myNums.add(3.14);
+    public static void copy(List<? extends Number> source, List< ? super Number > destiny) {
+        for (Number number : source) {
+            destiny.add(number);
+        }
+    }
 
-        Number x = myNums.get(0); // Erro de compilação
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.println(obj + "");
+        }
+        System.out.println();
     }
 }
